536337d13837ef9a34dcd7c46be453d959301337
paledger <pledgerw@calpoly.edu>
2016-10-12 12:29:12 -0700

    updated .travis.yml

--- AndroidStudioProjects/MyFirstApp/app/src/main/java/com/example/paulaanndroid/myfirstapp/MainActivity.java
+++ AndroidStudioProjects/MyFirstApp/app/src/main/java/com/example/paulaanndroid/myfirstapp/MainActivity.java
@@ -4,16 +4,21 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.WebView;
 import android.widget.EditText;
 import android.annotation.SuppressLint;
 import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
-public class MainActivity extends AppCompatActivity {
+class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //WebView myWebView = (WebView) findViewById(R.id.webview);
+        //String data = "<IFRAME SRC='http://server1.sky-map.org/skywindow?ra=00 42 32&de=41 19 53&zoom=4' WIDTH=400 HEIGHT=320' WIDTH=400 HEIGHT=320></IFRAME>";
+        //myWebView.loadData(data, "text/html", null);
+        //myWebView.loadUrl("http://server1.sky-map.org/?locale=EN");
     }
 
     public void sendMessage(View view) {
@@ -23,4 +28,5 @@ public class MainActivity extends AppCompatActivity {
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
     }
+
 }
