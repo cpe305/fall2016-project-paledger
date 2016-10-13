5389e1d6e5d35fcf4f0ae7cfcc85d18bdbbe6337
cevenhub <cevenmail@gmail.com>
2016-10-13 16:31:23 -0700

    Color Adjustments
    Changed color values of “Send” to a darker blue and the text box to
    “Pink/Purple” with higher brightness to the blue colors. The changes
    were made in colors.xml
    
    The background color was set to the same color as “Stargazing” by
    adding a line of code to the onCreate method in MainActity.java

--- app/src/main/java/com/example/paulaanndroid/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/paulaanndroid/myfirstapp/MainActivity.java
@@ -14,6 +14,7 @@ class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        android:background="#3F51B5"
         setContentView(R.layout.activity_main);
         //WebView myWebView = (WebView) findViewById(R.id.webview);
         //String data = "<IFRAME SRC='http://server1.sky-map.org/skywindow?ra=00 42 32&de=41 19 53&zoom=4' WIDTH=400 HEIGHT=320' WIDTH=400 HEIGHT=320></IFRAME>";
